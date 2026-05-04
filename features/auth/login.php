ee8d701d14efa77d89aa6b0c9465e34646e36c26
Azzazy6269 <mohamadelazzazy@gmail.com>
2026-05-04 23:34:50 +0300

    add title to login page

--- features/auth/login.php
+++ features/auth/login.php
@@ -7,6 +7,7 @@ unset($_SESSION['success']);
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
 <head>
+    <title>IRB-digital-system تسجيل الدخول </title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
